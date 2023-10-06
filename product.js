17d39c49c51a94bfe37e3e1e9a9a4ce24b12db8d
RedEstDeRetour <redisbackhere@gmail.com>
2023-10-06 19:54:09 +0200

    Ajout de la page d'achat

---
+++ product.js
@@ -0,0 +1,10 @@
+// JavaScript pour gérer les interactions de la page produit
+document.addEventListener('DOMContentLoaded', function() {
+    // Code pour ajouter le produit au panier au clic sur le bouton "Ajouter au Panier"
+    var addToCartButton = document.getElementById('add-to-cart');
+    addToCartButton.addEventListener('click', function() {
+      // Code pour ajouter le produit au panier
+      // Vous devrez implémenter cette fonctionnalité
+    });
+  });
+  

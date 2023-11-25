e03fbf152ab9f41106f93e8e04174abdd5cbb496
RedEstDeRetour <redisbackhere@gmail.com>
2023-11-25 14:00:16 +0100

    Ajouter au panier & Voir produit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function addProduct(imageSrc, title, description, price, elementID) {
+function addProduct(imageSrc, title, description, price, elementID, info_box_message, available) {
   var container = document.getElementById(elementID);
 
   var productDiv = document.createElement("div");
@@ -12,10 +12,24 @@ function addProduct(imageSrc, title, description, price, elementID) {
   image.src = imageSrc;
   imgDiv.appendChild(image);
 
-  var productInfoBox = document.createElement("div");
-  productInfoBox.classList.add("product-info-box");
-  productInfoBox.textContent = "INDISPONIBLE";
-  imgDiv.appendChild(productInfoBox);
+  if (info_box_message != null) {
+    var productInfoBox = document.createElement("div");
+    productInfoBox.classList.add("product-info-box");
+    productInfoBox.textContent = info_box_message;
+    imgDiv.appendChild(productInfoBox);
+  }
+
+  if (!available) {
+    var buyProduct = document.createElement("div");
+    buyProduct.classList.add("buy-product");
+    buyProduct.textContent = "Ajouter au panier";
+    imgDiv.appendChild(buyProduct);
+  }
+
+  var viewProdut = document.createElement("div");
+  viewProdut.classList.add("view-product");
+  viewProdut.textContent = "Voir le produit";
+  imgDiv.appendChild(viewProdut);
 
   var zoomDiv = document.createElement("div");
   zoomDiv.classList.add("zoom-icon");
